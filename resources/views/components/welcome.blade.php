71b4df22b31a5ae539970f8e641df0a8f4a7121f
Rogerio Almeida <rvalmeida.ti@gmail.com>
2023-09-15 05:51:07 -0300

    Implementado autenticação Jstream e Sanctum

---
+++ resources/views/components/welcome.blade.php
@@ -0,0 +1,3 @@
+<div class="p-6 lg:p-8 bg-white border-b border-gray-200">
+<h1>TESTES</h1>
+</div>
--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -14,6 +16,34 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::post('/sanctum/token', function (Request $request) {
+    $request->validate([
+        'email' => 'required|email',
+        'password' => 'required',
+        'device_name' => 'required',
+    ]);
+ 
+    $user = User::where('email', $request->email)->first();
+ 
+    if (! $user || ! Hash::check($request->password, $user->password)) {
+        throw ValidationException::withMessages([
+            'email' => ['The provided credentials are incorrect.'],
+        ]);
+    }
+    
+    $token = json_encode(['token'=> $user->createToken($request->device_name)->plainTextToken]);
+
+    return $token;
+});
+
+
+Route::middleware(['auth:sanctum'])->group(function () {
+    
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+
+    Route::get('/testes', function (Request $request) {
+        return 'testes';
+    });
 });
